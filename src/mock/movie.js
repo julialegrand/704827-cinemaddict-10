d4f9b4a33e56e4267618ad21cb18c40e112c2868
Julia <ggfox@mail.ru>
2020-01-22 22:01:55 +0300

    Fixed eslint error

--- src/mock/movie.js
+++ src/mock/movie.js
@@ -1,5 +1,3 @@
-import {RATING_MIN_VALUE, RATING_MAX_VALUE} from '../utils/const.js';
-
 const DESCRIPTION = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`;
 
 const RANDOM_LIMIT = 0.5;
