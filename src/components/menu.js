caa163500c36ec831ed1f5d9604297b7e14fccea
Julia <ggfox@mail.ru>
2020-01-24 01:31:26 +0300

    Личный проект: меняй-удаляй (Часть 1)

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,11 +1,12 @@
 import AbstractComponent from './abstract-component.js';
+import {FilterType} from '../utils/const.js';
 
 const createFiltersTemplate = (filters) => {
   return filters.map((filter) => {
-    const {title, href, count, isActive} = filter;
-    const activeClass = isActive ? `main-navigation__item--active` : ``;
-    const countTemplate = count ? `<span class="main-navigation__item-count">${count}</span>` : ``;
-    return `<a href="#${href}" class="main-navigation__item ${activeClass}">${title} ${countTemplate}</a>`;
+    const {name, count, checked} = filter;
+    const activeClass = checked ? `main-navigation__item--active` : ``;
+    const countTemplate = name !== FilterType.ALL ? `<span class="main-navigation__item-count">${count}</span>` : ``;
+    return `<a href="#" data-filter-type="${name}" class="main-navigation__item ${activeClass}">${name} ${countTemplate}</a>`;
   })
   .join(`\n`);
 };
@@ -15,7 +16,7 @@ const createMenuTemplate = (filters) => {
 
   return `<nav class="main-navigation">
             ${filtersTemplate}
-            <a href="#stats" class="main-navigation__item main-navigation__item--additional">Stats</a>
+            <a href="#stats" data-filter-type="Stats" class="main-navigation__item main-navigation__item--additional">Stats</a>
           </nav>`;
 };
 
@@ -24,8 +25,34 @@ export default class SiteMenu extends AbstractComponent {
   constructor(filters) {
     super();
     this._filters = filters;
+    this._currentFilterType = FilterType.ALL;
   }
   getTemplate() {
     return createMenuTemplate(this._filters);
+  }
+
+  setFilterChangeHandler(handler) {
+    this.getElement().addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+
+      if (evt.target.tagName !== `A`) {
+        return;
+      }
+
+      const filterType = evt.target.dataset.filterType;
+
+      if (this._currentFilterType === filterType) {
+        return;
+      }
+
+      this._currentFilterType = filterType;
+      this.getElement().querySelectorAll(`.main-navigation__item`).forEach((item) => {
+        if (item.classList.contains(`main-navigation__item--active`)) {
+          item.classList.remove(`main-navigation__item--active`);
+        }
+      });
+      evt.target.classList.add(`main-navigation__item--active`);
+      handler(this._currentFilterType);
+    });
   }
 }
--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 import PageController from './controllers/page.js';
+import FilterController from './controllers/filter';
 import {generateMovies} from './mock/movie.js';
-import {generateFilters} from './mock/filter.js';
+import MoviesModel from './models/movies.js';
 
 const COUNT_FILMS = 15;
 
 const movies = generateMovies(COUNT_FILMS);
-const filters = generateFilters(movies);
+const moviesModel = new MoviesModel();
+moviesModel.setMovies(movies);
 
-const mainController = new PageController(movies, filters);
+const mainElement = document.querySelector(`.main`);
 
+const filterController = new FilterController(mainElement, moviesModel);
+const mainController = new PageController(mainElement, moviesModel);
+
+filterController.render();
 mainController.render();
