da9d0bb7f6a18e49fd8c338e16c792e2b3a8460e
Julia <ggfox@mail.ru>
2020-01-29 20:33:19 +0300

    Content loading added

---
+++ src/components/content-loading.js
@@ -0,0 +1,17 @@
+import AbstractComponent from './abstract-component.js';
+
+const createContentLoadingTemplate = () => {
+  return (
+    `<section class="films">
+      <section class="films-list">
+        <h2 class="films-list__title">Loading...</h2>
+      </section>
+    </section>`
+  );
+};
+
+export default class ContentLoading extends AbstractComponent {
+  getTemplate() {
+    return createContentLoadingTemplate();
+  }
+}
